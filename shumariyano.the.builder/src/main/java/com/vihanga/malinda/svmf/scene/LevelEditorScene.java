bfe06e2003ac4dc3e04a234dd1ae2c49d7688e6a
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-05-10 10:33:58 +0530

    comment: commenting code for future references.

--- shumariyano.the.builder/src/main/java/com/vihanga/malinda/svmf/scene/LevelEditorScene.java
+++ shumariyano.the.builder/src/main/java/com/vihanga/malinda/svmf/scene/LevelEditorScene.java
@@ -253,4 +253,23 @@ public class LevelEditorScene extends Scene{
     }
 
 
+//    @Override
+//    public void update(Window window,
+//                       float delta) {
+//        System.out.println("Rate of frames per second: " + (1.0f / delta));
+//        if(!isChangingScene && keyListener.isKeyPressed(KeyEvent.VK_SPACE)){
+//            isChangingScene = true;
+//        }
+//
+//        if (isChangingScene && timeToChangeSceneThreshold>0){
+//            timeToChangeSceneThreshold -= delta;
+//            // RGB colour transition from black to white
+//        } else if(isChangingScene){
+//            // Change to level scene
+//            window.changeScene(1);
+//
+//        }
+//    }
+
+
 }
