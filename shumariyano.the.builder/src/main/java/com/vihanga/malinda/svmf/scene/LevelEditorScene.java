7bccdce62f32269795a7280cf6c697e879c2411f
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-05-10 10:31:23 +0530

    feat: adding vertex shader src.

--- shumariyano.the.builder/src/main/java/com/vihanga/malinda/svmf/scene/LevelEditorScene.java
+++ shumariyano.the.builder/src/main/java/com/vihanga/malinda/svmf/scene/LevelEditorScene.java
@@ -2,8 +2,17 @@ package com.vihanga.malinda.svmf.scene;
 
 import com.vihanga.malinda.svmf.listner.KeyListener;
 import com.vihanga.malinda.svmf.window.Window;
+import org.lwjgl.BufferUtils;
 
 import java.awt.event.KeyEvent;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL20.glGetShaderi;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
+import static org.lwjgl.opengl.GL30.glGenVertexArrays;
+
 
 public class LevelEditorScene extends Scene{
     private final KeyListener keyListener;
@@ -11,6 +20,67 @@ public class LevelEditorScene extends Scene{
     private boolean isChangingScene;
     private float timeToChangeSceneThreshold ;
 
+    private String vertexShaderSrc ="#version 330 core\n" +
+            "    layout (location=0) in vec3 aPos;\n" +
+            "    layout (location=1) in vec4 aColor;\n" +
+            "\n" +
+            "    out vec4 fColor;\n" +
+            "\n" +
+            "    void main(){\n" +
+            "        fColor = aColor;\n" +
+            "        gl_Position = vec4(aPos, 1.0);\n" +
+            "    }";
+
+    private String fragmentShaderSrc ="#version 330 core\n" +
+            "\n" +
+            "    in vec4 fColor;\n" +
+            "    out vec4 color;\n" +
+            "\n" +
+            "    void main(){\n" +
+            "        color =fColor;\n" +
+            "    }";
+
+    private int vertexId;
+    private int fragmentId;
+    private int shaderProgram;
+
+    private float[] vertexArray={
+            //position                    // color
+            0.5f,-0.5f,0.0f,              1.0f,0.0f,0.0f,1.0f, // Bottom right  [index of vertex 0]
+            -0.5f,0.5f,0.0f,              0.0f,1.0f,0.0f,1.0f, // Top Left      [index of vertex 1]
+            0.5f,0.5f,0.0f,               0.0f,0.0f,1.0f,1.0f, // Top right     [index of vertex 2]
+            -0.5f,-0.5f,0.0f,             1.0f,1.0f,0.0f,1.0f, // Bottom left   [index of vertex 3]
+    };
+
+    /***
+     * ==============================================
+     * IMPORTANT :: Must be in counter-clockwise order
+     *===============================================
+     */
+    private int[] elementArray ={
+            /*
+            K,L,P,J are 4 vertexes and square is made up of 2 triangles
+
+                    K        L
+                    x--------x
+                    |\       |         + 'A' triangle counter-clockwise order should be
+                    | \  (A) |          from vertex   L-> K -> P ( Top right -> Top left -> Bottom right)
+                    |  \     |
+                    |   \    |         + 'B' triangle counter-clockwise order should be
+                    |    \   |          from vertex P-> K -> J (Bottom right -> Top Left -> Bottom left)
+                    |     \  |
+                    | (B)  \ |
+                    |       \|
+                    x--------x
+                    J        P
+             */
+            2,1,0, // Top right triangle
+            0,1,3  // bottom left triangle
+    };
+
+    private int vertexArrayObjectId; // vaoId
+    private int vertexBufferObjectId; // vboId
+    private int elementBufferObjectId; //eboId
 
     public LevelEditorScene(KeyListener keyListener,boolean isChangingScene){
         System.out.println("Level Editor Scene created");
