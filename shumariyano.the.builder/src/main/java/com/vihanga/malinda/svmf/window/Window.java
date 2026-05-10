19443da05a23ac2ca5aacc172dd717960837fc33
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-05-10 10:29:23 +0530

    feat: initializing the scene before updating it.

--- shumariyano.the.builder/src/main/java/com/vihanga/malinda/svmf/window/Window.java
+++ shumariyano.the.builder/src/main/java/com/vihanga/malinda/svmf/window/Window.java
@@ -43,9 +43,11 @@ public class Window {
         switch (newScene){
             case 0:
                 currentScene = new LevelEditorScene(this.keyListener,false);
+                currentScene.init();
                 break;
             case 1:
                 currentScene = new LevelScene();
+                currentScene.init();
                 break;
             default:
                 assert false: "Unknown scene: " + newScene;
