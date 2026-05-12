30c3459dcfa8c7bb004fd43728092355edd8f9ad
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-05-12 07:22:02 +0530

    feat: add two constants which will be used on matrix conversion to one dimension array.

--- shumariyano.the.builder/src/main/java/com/vihanga/malinda/svmf/renderer/ShaderConstant.java
+++ shumariyano.the.builder/src/main/java/com/vihanga/malinda/svmf/renderer/ShaderConstant.java
@@ -6,4 +6,8 @@ public class ShaderConstant {
 
     public final static String TYPE_IDENTIFIER_PATTERN ="(#type)()+([a-zA-Z])+";
     public final static String TEST_PATTERN= "#type\\s+(vertex|fragment)";
+
+    public final static int BUFFER_CAPACITY_FOR_FOUR_DIMENSION_MATRIX=16;
+    public final static int BUFFER_CAPACITY_FOR_THREE_DIMENSION_MATRIX=9;
+
 }
